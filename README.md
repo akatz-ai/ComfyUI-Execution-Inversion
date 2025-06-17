6672add82932ae8a86b55c56d3eb81eef1714c57
akatz-ai <katzfeyalex+akatzgh@gmail.com>
2025-06-17 23:40:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Execution Inversion Comfy Nodes
+
+Contains nodes related to the new execution inversion engine in ComfyUI.
+
+Node pack originally from https://github.com/BadCafeCode/execution-inversion-demo-comfyui
---
+++ base_node.py
@@ -0,0 +1,28 @@
+NODE_NAME = "Execution Inversion 🔁"  # Can be changed to any desired prefix
+NODE_POSTFIX = ""
+
+# Base class for all nodes
+class BaseNode:
+    CATEGORY = NODE_NAME
+    
+    @classmethod
+    def get_category(cls):
+        return cls.CATEGORY
+    
+class LogicNode(BaseNode):
+    CATEGORY = f"{NODE_NAME}/Logic"
+    
+class FlowNode(BaseNode):
+    CATEGORY = f"{NODE_NAME}/Flow"
+    
+class UtilityNode(BaseNode):
+    CATEGORY = f"{NODE_NAME}/Utility"
+    
+class DebugNode(BaseNode):
+    CATEGORY = f"{NODE_NAME}/Debug"
+    
+class ListNode(BaseNode):
+    CATEGORY = f"{NODE_NAME}/Lists"
+    
+class LatentNode(BaseNode):
+    CATEGORY = f"{NODE_NAME}/Latent"
